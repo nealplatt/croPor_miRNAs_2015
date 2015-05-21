8ffee6d2cd5238a1ada015b57183a21e3d760d3b
Neal Platt <neal.platt@gmail.com>
2015-05-21 16:13:34 -0500

    inital commit, print HQ mature seqs

---
+++ bin/genHQseq.pl
@@ -0,0 +1,17 @@
+$i = 1;
+
+open MATURE, '>', hq_matureMirna.fas;
+open HAIRPIN, '>', hq_hairpin.fas;
+
+while(<>){
+    chomp;
+    ($uniqName, $knownName, $matureSeq, $hairpinSeq) = split "\t", $_;
+    if( $knownName eq "-" ){
+        print MATURE ">cro-mir-T$i\n$matureSeq\n";
+        print HAIRPIN ">cro-mir-T$i\n$hairpinSeq\n";
+    }else{
+        print MATURE ">cro-mir-K$i\n$matureSeq\n";
+        print HAIRPIN ">cro-mir-K$i\n$hairpinSeq\n";
+    }
+    $i++;
+}
